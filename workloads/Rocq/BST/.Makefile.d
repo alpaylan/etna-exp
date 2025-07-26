a815879b70db1527c688d83c1b9486bb771b8add
Alperen Keles <akeles@umd.edu>
2025-07-26 17:10:50 -0400

    automated commit: add workload 'Rocq/BST'

---
+++ workloads/Rocq/BST/.Makefile.d
@@ -0,0 +1,22 @@
+Src/Impl.vo Src/Impl.glob Src/Impl.v.beautified Src/Impl.required_vo: Src/Impl.v 
+Src/Impl.vos Src/Impl.vok Src/Impl.required_vos: Src/Impl.v 
+Src/Spec.vo Src/Spec.glob Src/Spec.v.beautified Src/Spec.required_vo: Src/Spec.v Src/Impl.vo
+Src/Spec.vos Src/Spec.vok Src/Spec.required_vos: Src/Spec.v Src/Impl.vos
+Strategies/BespokeGenerator.vo Strategies/BespokeGenerator.glob Strategies/BespokeGenerator.v.beautified Strategies/BespokeGenerator.required_vo: Strategies/BespokeGenerator.v Src/Impl.vo Src/Spec.vo
+Strategies/BespokeGenerator.vos Strategies/BespokeGenerator.vok Strategies/BespokeGenerator.required_vos: Strategies/BespokeGenerator.v Src/Impl.vos Src/Spec.vos
+Strategies/TypeBasedGenerator.vo Strategies/TypeBasedGenerator.glob Strategies/TypeBasedGenerator.v.beautified Strategies/TypeBasedGenerator.required_vo: Strategies/TypeBasedGenerator.v Src/Impl.vo Src/Spec.vo
+Strategies/TypeBasedGenerator.vos Strategies/TypeBasedGenerator.vok Strategies/TypeBasedGenerator.required_vos: Strategies/TypeBasedGenerator.v Src/Impl.vos Src/Spec.vos
+Strategies/SpecificationBasedGenerator.vo Strategies/SpecificationBasedGenerator.glob Strategies/SpecificationBasedGenerator.v.beautified Strategies/SpecificationBasedGenerator.required_vo: Strategies/SpecificationBasedGenerator.v Src/Impl.vo Src/Spec.vo
+Strategies/SpecificationBasedGenerator.vos Strategies/SpecificationBasedGenerator.vok Strategies/SpecificationBasedGenerator.required_vos: Strategies/SpecificationBasedGenerator.v Src/Impl.vos Src/Spec.vos
+Strategies/TypeBasedFuzzer.vo Strategies/TypeBasedFuzzer.glob Strategies/TypeBasedFuzzer.v.beautified Strategies/TypeBasedFuzzer.required_vo: Strategies/TypeBasedFuzzer.v Src/Impl.vo Src/Spec.vo
+Strategies/TypeBasedFuzzer.vos Strategies/TypeBasedFuzzer.vok Strategies/TypeBasedFuzzer.required_vos: Strategies/TypeBasedFuzzer.v Src/Impl.vos Src/Spec.vos
+Runners/BespokeGenerator_test_runner.vo Runners/BespokeGenerator_test_runner.glob Runners/BespokeGenerator_test_runner.v.beautified Runners/BespokeGenerator_test_runner.required_vo: Runners/BespokeGenerator_test_runner.v Strategies/BespokeGenerator.vo
+Runners/BespokeGenerator_test_runner.vos Runners/BespokeGenerator_test_runner.vok Runners/BespokeGenerator_test_runner.required_vos: Runners/BespokeGenerator_test_runner.v Strategies/BespokeGenerator.vos
+Runners/BespokeGenerator_sampler.vo Runners/BespokeGenerator_sampler.glob Runners/BespokeGenerator_sampler.v.beautified Runners/BespokeGenerator_sampler.required_vo: Runners/BespokeGenerator_sampler.v Strategies/BespokeGenerator.vo Src/Impl.vo
+Runners/BespokeGenerator_sampler.vos Runners/BespokeGenerator_sampler.vok Runners/BespokeGenerator_sampler.required_vos: Runners/BespokeGenerator_sampler.v Strategies/BespokeGenerator.vos Src/Impl.vos
+Runners/TypeBasedGenerator_test_runner.vo Runners/TypeBasedGenerator_test_runner.glob Runners/TypeBasedGenerator_test_runner.v.beautified Runners/TypeBasedGenerator_test_runner.required_vo: Runners/TypeBasedGenerator_test_runner.v Strategies/TypeBasedGenerator.vo
+Runners/TypeBasedGenerator_test_runner.vos Runners/TypeBasedGenerator_test_runner.vok Runners/TypeBasedGenerator_test_runner.required_vos: Runners/TypeBasedGenerator_test_runner.v Strategies/TypeBasedGenerator.vos
+Runners/SpecificationBasedGenerator_test_runner.vo Runners/SpecificationBasedGenerator_test_runner.glob Runners/SpecificationBasedGenerator_test_runner.v.beautified Runners/SpecificationBasedGenerator_test_runner.required_vo: Runners/SpecificationBasedGenerator_test_runner.v Strategies/SpecificationBasedGenerator.vo
+Runners/SpecificationBasedGenerator_test_runner.vos Runners/SpecificationBasedGenerator_test_runner.vok Runners/SpecificationBasedGenerator_test_runner.required_vos: Runners/SpecificationBasedGenerator_test_runner.v Strategies/SpecificationBasedGenerator.vos
+Runners/TypeBasedFuzzer_test_runner.vo Runners/TypeBasedFuzzer_test_runner.glob Runners/TypeBasedFuzzer_test_runner.v.beautified Runners/TypeBasedFuzzer_test_runner.required_vo: Runners/TypeBasedFuzzer_test_runner.v Strategies/TypeBasedFuzzer.vo
+Runners/TypeBasedFuzzer_test_runner.vos Runners/TypeBasedFuzzer_test_runner.vok Runners/TypeBasedFuzzer_test_runner.required_vos: Runners/TypeBasedFuzzer_test_runner.v Strategies/TypeBasedFuzzer.vos
